3729807eefca31dd75e94548e6d4ed466489c60a
john7325 <john7325@uw.edu>
2022-11-24 11:57:05 -0800

    Fetching post data from server

--- client/src/components/FindSchool.js
+++ client/src/components/FindSchool.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react"; 
+import React, { useState, useEffect } from "react"; 
 import Button from "@mui/material/Button";
 import { SchoolPost } from "./../widgets/Post.js"; 
 import "./../stylesheets/FindSchool.css";
 
 function FindSchool() {
   const [popupStatus, setPopupStatus] = useState(false); 
+  const [schoolData, setSchoolData] = useState([]); 
+
+  useEffect(() => {
+    fetch("/api/posts/")
+  }, []);
 
   const buttonStyle = {
     fontSize: "1.15rem",
@@ -23,13 +28,13 @@ function FindSchool() {
 
       {/* Page posts */}
       <section className="school-posts" aria-labelledby="school posts">
-         <SchoolPost popupStatus={popupStatus} setPopupStatus={setPopupStatus} />
+         {/* <SchoolPost popupStatus={popupStatus} setPopupStatus={setPopupStatus} />
          <SchoolPost />
          <SchoolPost />
          <SchoolPost />
          <SchoolPost />
          <SchoolPost />
-         <SchoolPost />
+         <SchoolPost /> */}
       </section> 
 
       <h1 style={popupStatus ? {display: "block"} : {display: "none"}}>Display Popup</h1>
